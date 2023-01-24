cd06df5f67a4be857d237b26ebd7063e6c28aab5
code4always <parwiz.rajabi@powercoders.org>
2023-01-24 16:22:48 +0100

    Video added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
+const impressum = document.querySelector('#open');
 const openImpressum = document.querySelector('impressum');
+const closeImpressum = document.querySelector('close');
+
+openImpressum.addEventListener('click', () => {
+    impressum.showImpressum();
+})
